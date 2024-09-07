8cfe7d32e2a1deda0308118da750eaf55b019c0c
jaery <jaeryong95@gmail.com>
2024-09-07 21:30:53 +0900

    단순 코딩으로 system log 찍어보기

---
+++ src/class1/ClassStart1.java
@@ -0,0 +1,16 @@
+package class1;
+
+public class ClassStart1 {
+    public static void main(String[] args) {
+        String student1Name = "학생1";
+        int student1Age = 18;
+        int student1Grade = 90;
+
+        String student2Name = "학생2";
+        int student2Age = 17;
+        int student2Grade = 16;
+
+        System.out.println("이름: " + student1Name + " 나이: " + student1Age + " 성적: " + student1Grade + "점");
+        System.out.println("이름: " + student2Name + " 나이: " + student2Age + " 성적: " + student2Grade + "점");
+    }
+}
