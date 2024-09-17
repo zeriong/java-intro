989cdee3b6d8ad4710625be920a9955af904ebbe
jaery <jaeryong95@gmail.com>
2024-09-17 18:27:19 +0900

    객체지향이 아닌 코드를 객체지향으로 리팩토링하는 연습

---
+++ src/oop1/ex/Rectangle.java
@@ -0,0 +1,18 @@
+package oop1.ex;
+
+public class Rectangle {
+    int width = 5;
+    int height = 8;
+
+    int calculateArea() {
+        return width * height;
+    }
+
+    int calculatePerimeter() {
+        return 2 * (width + height);
+    }
+
+    boolean isSquare() {
+        return width == height;
+    }
+}
