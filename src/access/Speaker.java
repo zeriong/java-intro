53a990f54bef6538929df31e9f27c42fc974c72c
jaery <jaeryong95@gmail.com>
2024-09-17 18:44:31 +0900

    private 접근제어자에 대한 개념과 예시를 통한 학습

---
+++ src/access/Speaker.java
@@ -0,0 +1,33 @@
+package access;
+
+public class Speaker {
+    public int volume;
+
+    Speaker(int volume) {
+        this.volume = volume;
+    }
+
+    void volumeUp() {
+        if (volume >= 100) {
+            System.out.println("음량을 증가시킬 수 없습니다. 최대 음량입니다.");
+        } else {
+            volume += 10;
+            System.out.println("음량을 10 증가합니다.");
+        }
+    }
+
+    void volumeDown() {
+        if (volume < 1) {
+            System.out.println("음량을 감소시킬 수 없습니다. 최소 음량입니다.");
+        } else {
+            volume -= 10;
+            System.out.println("음량을 10 감소합니다.");
+        }
+        volume -= 10;
+        System.out.println("volumeDown 호출");
+    }
+
+    void showVolume() {
+        System.out.println("현재 음량: " + volume);
+    }
+}
---
+++ src/access/SpeakerMain.java
@@ -0,0 +1,18 @@
+package access;
+
+public class SpeakerMain {
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker(90);
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        speaker.volumeUp();
+        speaker.showVolume();
+
+        speaker.volume = 100;
+        speaker.showVolume();
+
+    }
+}
