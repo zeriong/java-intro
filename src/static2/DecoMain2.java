4d069a907f7431120b0aefe64ff6d4feb4a163c6
zeriong <jaeryong95@gmail.com>
2024-09-18 15:24:45 +0900

    DecoMain1, 2: static method에 대한 학습

---
+++ src/static2/DecoMain2.java
@@ -0,0 +1,10 @@
+package static2;
+
+public class DecoMain2 {
+    public static void main(String[] args) {
+        String s = "Hello Java";
+
+        System.out.println("before: " + s);
+        System.out.println("after: " + DecoUtil2.deco(s));
+    }
+}
