b02b92e8cc77d03a02552ab10ccb237e386b27f7
zeriong <jaeryong95@gmail.com>
2024-09-19 08:28:02 +0900

    상속에 대한 이해도를 높이기 위한 추가 예제코드 작성

---
+++ src/extends1/ex3/CarMain.java
@@ -0,0 +1,19 @@
+package extends1.ex3;
+
+public class CarMain {
+
+    public static void main(String[] args) {
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.move();
+        electricCar.charge();
+
+        GasCar gasCar = new GasCar();
+        gasCar.move();
+        gasCar.fillUp();
+
+        HydrogenCar hydrogenCar = new HydrogenCar();
+        hydrogenCar.move();
+        hydrogenCar.fillHydrogen();
+        hydrogenCar.openDoor();
+    }
+}
